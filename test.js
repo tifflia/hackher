6c8233878dfb8aa4da166c8c5548847008446939
mharada337 <mharada27.amherst.edu>
2024-02-18 10:57:22 -0500

    Co-authored-by: Tiffany Liang <tifflia@users.noreply.github.com>

--- test.js
+++ test.js
@@ -1,9 +1,55 @@
 function practice() {
-    window.location.href = "practice.html";
+    window.location.href = "question1.html";
 }
 
 function quiz() {
     window.location.href = "quiz.html";
 }
 
-const letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+function correct() {
+    window.location.href = "question2.html";
+}
+
+function correct2() {
+    window.location.href = "question3.html";
+}
+
+function correct3() {
+    window.location.href = "question4.html";
+}
+
+function wrong1() {
+    document.getElementById("option2").style.backgroundColor = "#E07A5F";
+}
+
+function wrong2() {
+    document.getElementById("option3").style.backgroundColor = "#E07A5F";
+}
+
+function wrong3() {
+    document.getElementById("option4").style.backgroundColor = "#E07A5F";
+}
+
+function wrong4() {
+    document.getElementById("option1").style.backgroundColor = "#E07A5F";
+}
+
+function wrong5() {
+    document.getElementById("option3").style.backgroundColor = "#E07A5F";
+}
+
+function wrong6() {
+    document.getElementById("option4").style.backgroundColor = "#E07A5F";
+}
+
+function wrong7() {
+    document.getElementById("option1").style.backgroundColor = "#E07A5F";
+}
+
+function wrong8() {
+    document.getElementById("option2").style.backgroundColor = "#E07A5F";
+}
+
+function wrong9() {
+    document.getElementById("option4").style.backgroundColor = "#E07A5F";
+}
