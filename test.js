d04de7b1b997beaa5d745cf4fb9836e7551613cc
mharada337 <mharada27.amherst.edu>
2024-02-18 09:15:40 -0500

    practice

--- test.js
+++ test.js
@@ -5,3 +5,5 @@ function practice() {
 function quiz() {
     window.location.href = "quiz.html";
 }
+
+const letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "S", "T", "U", "V", "W", "X", "Y", "Z"];
